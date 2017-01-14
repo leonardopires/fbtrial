7046a7f3251dcbeb7d574c3e4a3e51ededd3dca4
Leonardo Thome Pires <leonardo@weedseeker.net>
2017-01-14 00:53:59 -0200

    Renamed assemblies and default namespaces. Created test project for the repository layer.

---
+++ Source/Journals.Repository.Tests/JournalRepositoryTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Journals.Repository.Tests
+{
+    public class JournalRepositoryTest
+    {
+
+        [Fact]
+        public void GetJournalList()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
