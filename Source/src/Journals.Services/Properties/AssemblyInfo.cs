89ff82e83b7220c011110f4397b2874dce5a75fa
Leonardo Thome Pires <leonardo@weedseeker.net>
2017-01-18 02:02:43 -0200

    Started the work of splitting the projects into smaller, less interdependent layers.

---
+++ Source/src/Journals.Services/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Journals.Services")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ecf3affa-cb7b-4f07-969f-571e377e6d39")]
---
+++ Source/src/LP.Framework.Data/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LP.Framework.Data
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
